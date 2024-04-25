3d14bfeb4ee31064b0efaa276455b80c88a94fa0
syairaarif <putrisyafira4444@gmail.com>
2024-04-25 10:30:41 +0700

    melanjutkan program duntuk int(fungsi) main

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -159,9 +159,37 @@ int main(){
                     system("cls");
                     break;
                 }
+
+                int nim;
+                cout << "Masukan NIM: ";
+                cin >> nim;
+                if (deleteNode(nim)) {
+                    cout << "nim: " << nim << " berhasil dihapus" << endl;
+                    system("pause");
+                    system("cls");
+                }
+                else
+                    cout << "Data tidak ditemukan" << endl;
+                break;
+            case 3: 
+                traverse();
+                break;
+            case 4:
+                searchData();
+                break;
+            case 5:
+                break;
+            default:
+                cout << "Pilihan tidak ada" << endl;
+                break;
             }
         }
-    }
+        catch (exception e)
+        {
+            cout << "Terjadi Kesalahan" << endl;
+        }
+
+    } while (pilihan != 5);
 }
 
 
